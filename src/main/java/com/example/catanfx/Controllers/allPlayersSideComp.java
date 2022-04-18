e82b65a68f8423e58c1339331fbe24fb45d03cb7
Tomin <tchazhikat@gmail.com>
2022-04-18 11:50:36 -0500

    New allPlayersSideComp graphic for left side of main game board

---
+++ src/main/java/com/example/catanfx/Controllers/allPlayersSideComp.java
@@ -0,0 +1,54 @@
+package com.example.catanfx.Controllers;
+import com.example.catanfx.GamePieces.GameState;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class allPlayersSideComp implements Initializable {
+
+    @FXML
+    private Pane pane0;
+    @FXML
+    private Pane pane1;
+    @FXML
+    private Pane pane2;
+    @FXML
+    private Pane pane3;
+    @FXML
+    private Rectangle rect0;
+    @FXML
+    private Rectangle rect1;
+    @FXML
+    private Rectangle rect2;
+    @FXML
+    private Rectangle rect3;
+    @FXML
+    private Text text0;
+    @FXML
+    private Text text1;
+    @FXML
+    private Text text2;
+    @FXML
+    private Text text3;
+    @FXML
+    private TextArea textArea0;
+    @FXML
+    private TextArea textArea1;
+    @FXML
+    private TextArea textArea2;
+    @FXML
+    private TextArea textArea3;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+       // if(GameState.getAllPlayers().size()==3){pane3.setVisible(false);}
+
+    }
+
+}
