3e973e5867634b8814f45b9d43c39803a560f922
kattyman <tchazhikat@gmail.com>
2022-04-20 12:17:19 -0500

    Dice methods are static

--- src/main/java/com/example/catanfx/GamePieces/Misc/Dice.java
+++ src/main/java/com/example/catanfx/GamePieces/Misc/Dice.java
@@ -2,21 +2,21 @@ package com.example.catanfx.GamePieces.Misc;
 
 public class Dice {
 
-    private int number, number0;
+    private static int number, number0;
 
     public Dice() {
         number = 0;
         number0 = 0;
     }
 
-    public int getRolledNum() {
+    public static int getRolledNum() {
 
         number = (int)Math.random()*(6)+1;
         number0 = (int)Math.random()*(6)+1;
         return number + number0;
     }
 
-    public int[] rollDice() {
+    public static int[] rollDice() {
         int[] num = new int[2];
         num[0] = number;
         num[1] = number0;
