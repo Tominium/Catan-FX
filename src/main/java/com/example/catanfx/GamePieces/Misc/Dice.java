febe3dbbc690209f3fff77c51672f8942b3c5523
Tomin <tchazhikat@gmail.com>
2022-04-24 19:30:58 -0500

    Fixed Dice

--- src/main/java/com/example/catanfx/GamePieces/Misc/Dice.java
+++ src/main/java/com/example/catanfx/GamePieces/Misc/Dice.java
@@ -10,16 +10,15 @@ public class Dice {
     }
 
     public static int getRolledNum() {
-
-        number = (int)Math.random()*(6)+1;
-        number0 = (int)Math.random()*(6)+1;
         return number + number0;
     }
 
     public static int[] rollDice() {
         int[] num = new int[2];
-        num[0] = number;
-        num[1] = number0;
+        num[0] = (int)(Math.random()*6)+1;
+        num[1] = (int)(Math.random()*6)+1;
+        number=num[0];
+        number0=num[1];
         return num;
     }
 }
