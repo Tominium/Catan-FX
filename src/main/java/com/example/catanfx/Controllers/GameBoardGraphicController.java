9b0233c4f0d251d45fc88804c659a0be5ebe3cbc
SlHSCS301 <zmkcanada@gmail.com>
2022-05-03 12:17:59 -0500

    bank button added on Trade panel

--- src/main/java/com/example/catanfx/Controllers/GameBoardGraphicController.java
+++ src/main/java/com/example/catanfx/Controllers/GameBoardGraphicController.java
@@ -1,4 +1,34 @@
 package com.example.catanfx.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+
 public class GameBoardGraphicController {
+    @FXML
+    private AnchorPane anchorPane1;
+   @FXML
+    private AnchorPane anchorPane2;
+   @FXML
+    private AnchorPane anchorPane3;
+
+    public void initialize() {
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("test.fxml"));
+//        Parent root = loader.load();
+//        TestController controller = loader.<TestController>getController();
+//        controller.setData(data);
+    }
+
+    public AnchorPane getAP1() {
+        return anchorPane1;
+    }
+
+    public AnchorPane getAP3() {
+        return anchorPane3;
+    }
+
+
+
+
 }
