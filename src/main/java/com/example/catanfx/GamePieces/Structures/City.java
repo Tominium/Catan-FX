ce126ec3ec106b3fb729a9a407c804e4c53fc594
Tomin <tchazhikat@gmail.com>
2022-05-04 17:51:05 -0500

    setColor Method

--- src/main/java/com/example/catanfx/GamePieces/Structures/City.java
+++ src/main/java/com/example/catanfx/GamePieces/Structures/City.java
@@ -1,13 +1,15 @@
 package com.example.catanfx.GamePieces.Structures;
 
-import com.example.catanfx.GamePieces.Structures.Structure;
+import com.example.catanfx.Controllers.GameBoardController;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
-import java.awt.*;
 
 public class City extends Structure {
 
     public City(String type, String color, ImageView image) {
         super(type, color, image);
     }
+
+    public void setColor(String c){color = c; image.setImage(new Image(GameBoardController.class.getResource("/Assets/City/" + color + ".png").toExternalForm()));}
+
 }
